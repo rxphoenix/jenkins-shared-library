bf73427f7b344d5af3caf4f3ab52c1493a3bff44
Mathieu Couture <math.couture@gmail.com>
2017-09-08 13:18:37 -0400

    Commit

--- src/ca/qc/inspq/jenkins/AnsibleReader.groovy
+++ src/ca/qc/inspq/jenkins/AnsibleReader.groovy
@@ -1,9 +1,9 @@
+package ca.qc.inspq.jenkins
+
 @GrabResolver(name='mavenCentral', root='https://mvnrepository.com')
 @Grab(group='org.yaml', module='snakeyaml', version='1.18')
 
 import org.yaml.snakeyaml.Yaml
-
-package ca.qc.inspq.jenkins
 
 class AnsibleReader implements Serializable {
     def script
